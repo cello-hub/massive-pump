d5ef0361e04e9c9b8c6721c492d22b1e245f7a4e
lybenson <gwanbit@gmail.com>
2024-03-05 23:57:31 +0800

    feat: remove testcode

--- src/accouts/deposit.ts
+++ src/accouts/deposit.ts
@@ -52,24 +52,3 @@ export const depositToCex = async ({
       console.log(`${account.address} 充值交易成功`)
     })
 }
-
-const mnemonic = process.env.MNEMONIC || ''
-
-const accounts = generateAccounts(mnemonic, 100)
-
-const main = async () => {
-  const start = 1
-  for (let i = start; i < accounts.length; i++) {
-    console.log(`index=${i}------------------`)
-
-    await depositToCex({
-      account: accounts[i],
-      chain: opBNB,
-      rpc: 'https://opbnb-mainnet-rpc.bnbchain.org',
-      target: '0x156a909b045c3a31bf2a7e4554b7e458be869dc9',
-      value: parseEther('0.0097')
-    })
-  }
-}
-
-main()
